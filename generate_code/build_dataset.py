c92e09f3231a9b6e33b783bf5081be768fff320e
timothee.strouk <timothee.strouk@epita.fr>
2024-02-24 17:00:22 +0100

    feat(generate_code): generate prompt and build dataset adapted for new_dataset and made a bit more generic
    let's run better

--- generate_code/build_dataset.py
+++ generate_code/build_dataset.py
@@ -1,10 +1,31 @@
 from datasets import Dataset
+from generate_prompt import generate_prompt
+
+def build_generated_captions(code_dataset, llm, hugging_faces=True):
+    code_dataset["caption"] = []
+    prompt_generator = generate_caption(code_dataset, f"Knowing the program language is {lang} generate a detailed caption for this program: \n{codes}")
+    for i in code_dataset:
+        prom = prompt_generator.next()
+        answer = llm(prompt)
+        code_dataset["caption"].append(answer)
+    return code_dataset if not(hugging_faces) else Dataset.from_dict(code_dataset)
+
+def build_generated_codes(code_dataset, llm, hugging_faces=True):
+    code_dataset["generated_code"] = []
+    prompt_generator = generate_prompt(code_dataset, f"Generate a program in {lang} from this caption:\n {caption}"
+)
+    for i in code_dataset:
+        prom = prompt_generator.next()
+        answer = llm(prompt)
+        code_dataset["generated_code"].append(answer)
+    return code_dataset if not(hugging_faces) else Dataset.from_dict(code_dataset)
 
 def build_generated_dataset(code_dataset, llm, hugging_faces=True):
-    prompts = generate_prompt(code_dataset, len(code_dataset))
     code_dataset["generated_answer"] = []
-    for p in prompts:
-        answer = llm(p)
+    prompt_generator = generate_prompt(code_dataset, f"Knowing the code is in {lang}: \n{question}\ncontext:\n{code}")
+    for i in code_dataset:
+        prom = prompt_generator.next()
+        answer = llm(prompt)
         code_dataset["generated_answer"].append(answer)
     return code_dataset if not(hugging_faces) else Dataset.from_dict(code_dataset)
 
--- generate_code/generate_prompt.py
+++ generate_code/generate_prompt.py
@@ -1,30 +1,39 @@
 from load_dataset import load_dataset
 
-def generate_caption(json_dataset, num_prompt : int, prompt_template: fstring):
-    codes = json_dataset["code"]
+# f"Knowing the program language is {lang} generate a detailed caption for this program: \n{codes}"
+def generate_caption(json_dataset, prompt_template):
+    codes = json_dataset["generated_code"]
     lang = json_dataset["language"]
-    # Write the code associated with this caption
-    prompt = f"Knowing the program language is {lang} generate a detailed caption for this program: \n{codes}"
-    return prompt
-
-def generate_code_from_caption(json_dataset, num_prompt : int, prompt_template: fstring):
-    caption = json_dataset["caption"]
-    codes = json_dataset["code"]
-    lang = json_dataset["language"]
-    # Write the code associated with this caption
-    prompt = f"Generate a program in {lang} from this caption:\n {caption}"
-    return prompt
-
-def generate_prompt(json_dataset, num_prompt : int, prompt_template: fstring):
-    questions = json_dataset["question"]
-    codes = json_dataset["code"]
-    lang = json_dataset["lang"]
-    prompt_list = []
-    for i in range(num_prompt):
+    for code, lang in zip(codes, langs):
         # Write the code associated with this caption
-        prompt = f"Knowing the code is in {lang}: \n{questions[i]}\ncontext:\n{codes[i]}"
-        prompt_list.append(prompt)
-    return prompt_list
-#json = load_dataset("python", "dev", 20)
-#prompts = generate_prompt(json, 1)
-#print(prompts[0])
+        prompt = prompt_template.format(lang, code)
+        yield prompt
+
+# f"Generate a program in {lang} from this caption:\n {caption}"
+def generate_code_from_caption(json_dataset, prompt_template):
+    captions = json_dataset["caption"]
+    langs = json_dataset["language"]
+    for caption, lang in zip(captions, langs):
+        # Write the code associated with this caption
+        prompt = prompt_template.format(lang, caption)
+        yield prompt
+
+# f"Knowing the code is in {lang}: \n{question}\ncontext:\n{code}"
+def generate_prompt(json_dataset, prompt_template):
+    questions = json_dataset["question"]
+    codes = json_dataset["generated_code"]
+    langs = json_dataset["lang"]
+    for question, code, lang in zip(questions, codes, langs):
+        # Write the code associated with this caption
+        prompt = prompt_template.format(lang, question, code)
+        yield prompt
+
+#res = generate_prompt({"code" : ["things"], "question" : ["rnvirn ?"], "lang": ["fr"]}, "Knowing the code is in {}: \n{}\ncontext:\n{}")
+#for i in res:
+#    print(i)
+#res = generate_prompt({"caption" : ["rnvirn ?"], "lang": ["fr"]}, "Generate a program in {} from this caption:\n {}{}{}")
+#for i in res:
+#    print(i)
+# json = load_dataset("python", "dev", 20)
+# prompts = generate_prompt(json, 1)
+# print(prompts[0])
